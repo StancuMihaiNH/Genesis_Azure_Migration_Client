cca3f8f27513cfd856294cfc7834b1a168c86177
StancuMihaiNH <mihai.stancu@northhighland.com>
2024-08-06 18:46:36 +0100

    123

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,5 @@ export const Constants = {
     NEXT_PUBLIC_ACCESS_TOKEN_KEY: "accessToken",
     NEXT_PUBLIC_GRAPHQL_URL: "http://20.67.71.155",
     NEXT_PUBLIC_MESSAGE_API_URL: "http://20.13.45.40:8000/chat_stream/",
-    NEXT_PUBLIC_CLIENT_URL: "https://ai-coe-client.azurewebsites.net",
+    NEXT_PUBLIC_CLIENT_URL: "https://ai-coe-client-2.azurewebsites.net/",
 };
